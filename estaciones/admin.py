e1ca76bc388e5cebcd289681035e83d29b797e7e
Carlos Castellanos Vera <ccverak@gmail.com>
2013-03-28 00:11:11 +0100

    Multiples mejoras

--- estaciones/admin.py
+++ estaciones/admin.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-#
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 from django.forms import ModelForm
 from wysiwyg import ElrteWidget
-# from models import Estacion - creo que este esta de más
 
 from django.contrib.flatpages.models import FlatPage
 from django.contrib.flatpages.admin import FlatPageAdmin as FlatPageAdminOld
 
 from django.db import models
-from estaciones.models import Estacion, NivelSocioEconomico, NivelEdadTarget, \
-	FrecuenciaCobertura, Provincia #,User
+from estaciones.models import Estacion, NivelEdadTarget, \
+	FrecuenciaCobertura, Provincia, Cliente
 
 
 class FlatPageAdmin(FlatPageAdminOld):
@@ -23,40 +24,34 @@ class EstacionAdmin(admin.ModelAdmin):
     }
 
 	fieldsets = (("Datos de las estaciones de radio", {
-		'fields' : ('nombre', 'descripcion', 'categorias','logo', 'nivel_socioeconomico')
+		'fields' : ('nombre', 'descripcion', 'logo', 'categorias', 'en_promocion_desde', 'nivel_socioeconomico', 'niveles_edad_target', 'cobertura_frecuencias')
 		}), 
 	)
-	list_filter = ('nivel_socioeconomico__tipo','categorias__name')
+	list_filter = ('nivel_socioeconomico','categorias__name', 'en_promocion_desde', 'niveles_edad_target__rango_edad', 'cobertura_frecuencias__provincia__provincia', 'cobertura_frecuencias__provincia__region')
 	search_fields = ('nombre',
 					'slug',
 					'descripcion',
-					'nivel_socioeconomico__tipo'
-					'en_promocion_desde'
+					'nivel_socioeconomico',
+					'cobertura_frecuencias__provincia__region'
 					)
 	save_as = True
 	list_per_page = 10
-	list_display = ('nombre', 'slug', )#Falta logo
+	list_display = ('nombre', 'slug', 'en_promocion_desde')#Falta logo
 	list_display_links = ('nombre', 'slug',)
-	raw_id_fields = ['nivel_socioeconomico']
-	related_lookup_fields = {'m2m':['nivel_socioeconomico']}
-	#filter_horizontal = ('nivel_socioeconomico',)
-	#form = EstacionForm
+	raw_id_fields = ['niveles_edad_target', 'cobertura_frecuencias']
+	related_lookup_fields = {'m2m':['niveles_edad_target', 'cobertura_frecuencias']}
+	
 
-class NivelSocioEconomicoAdmin(admin.ModelAdmin):
-	list_display = ('tipo',)
-	search_fields = ('tipo',)
-	list_per_page = 10
-
+#TODO: Candidato a desaparecer
 class NivelEdadTargetAdmin(admin.ModelAdmin):
-	list_display = ('rango_edad', 'nivel_socioeconomico')
-	search_fields = ('rango_edad', 'nivel_socioeconomico__tipo')
-	list_filter = ('nivel_socioeconomico__tipo',)
+	list_display = ('rango_edad',)
+	search_fields = ('rango_edad',)
 	list_per_page = 10
 
 class FrecuenciaCoberturaAdmin(admin.ModelAdmin):
-	list_display = ('frecuencia', 'modulacion', 'provincia' , 'nivel_edad_target')
-	search_fields = ('frecuencia', 'modulacion', 'provincia__provincia', 'nivel_edad_target__rango_edad')
-	list_filter = ('provincia__provincia', 'nivel_edad_target__rango_edad')
+	list_display = ('frecuencia', 'modulacion', 'provincia')
+	search_fields = ('frecuencia', 'modulacion', 'provincia__provincia')
+	list_filter = ('provincia__provincia',)
 	list_per_page = 10
 
 class ProvinciaAdmin(admin.ModelAdmin):
@@ -64,11 +59,22 @@ class ProvinciaAdmin(admin.ModelAdmin):
 	search_fields = ('codigo', 'provincia', 'region')
 	list_per_page = 10
 
+class ClienteEmbebido(admin.StackedInline):
+    model = Cliente
+    can_delete = False
+    verbose_name_plural = 'clientes'
+
+# Define a new User admin
+class UserAdmin(UserAdmin):
+    inlines = (ClienteEmbebido, )
+
 admin.site.register(Estacion, EstacionAdmin)
-admin.site.register(NivelSocioEconomico, NivelSocioEconomicoAdmin)
 admin.site.register(NivelEdadTarget, NivelEdadTargetAdmin)
 admin.site.register(FrecuenciaCobertura, FrecuenciaCoberturaAdmin)
 admin.site.register(Provincia, ProvinciaAdmin)
 
 admin.site.unregister(FlatPage)
 admin.site.register(FlatPage, FlatPageAdmin)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- estaciones/forms.py
+++ estaciones/forms.py
@@ -3,6 +3,9 @@ from __future__ import with_statement
 
 from django import forms
 from django.conf import settings
+from estaciones.models import Estacion
+import datetime
+from django.utils.datetime_safe import date
 
 class ContactForm(forms.Form):
     """
@@ -14,4 +17,10 @@ class ContactForm(forms.Form):
     phone = forms.CharField(label='Teléfono')
     message = forms.CharField(label='Mensaje', widget=forms.Textarea)
     
-    
+# class EstacionForm(forms.ModelForm):
+# 	class Meta:
+#         model = Estacion
+
+#     def clean_en_promocion_desde(self):
+#     	en_promocion_desde = self.cleaned_data['en_promocion_desde']
+    	
